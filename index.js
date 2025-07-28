9f5ed32abec0278ffebaa60efcd99307a0c55803
Sathish Sharma <dontharajusathish@gmail.com>
2025-07-28 22:28:36 +0530

    first commit

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const bodyParser = require("body-parser");
 app.set("view engine", "ejs");
 app.use(express.static('public'));
 app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
 var items =[];
  app.get("/", function(req, res) {
 res.render("list",{ejes :items})
@@ -12,10 +14,24 @@ res.render("list",{ejes :items})
 
 app.post("/",function(req,res){
     var item =req.body.ele1;
-    items.push(item);
-    res.redirect("/");
+   items.push(item);
+   res.redirect("/");
 });
 
+app.delete("/delete", function (req, res) {
+  const taskToDelete = req.body.task;
+  items = items.filter(item => item !== taskToDelete);
+  res.status(200).json({ success: true });
+});
+
+app.put("/edit", function(req, res) {
+  const { oldTask, newTask } = req.body;
+  const index = items.indexOf(oldTask);
+  if (index !== -1) {
+    items[index] = newTask.trim();
+    return res.status(200).json({ success: true });
+  }
+});
  app.listen(4000, function(){
  console.log("Server started on port 4000");
  });
